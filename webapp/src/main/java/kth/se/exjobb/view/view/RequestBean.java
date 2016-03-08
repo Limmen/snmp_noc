9143205b530b3f413fd80016af0fba43af708a42
limmen <kimham@kth.se>
2016-03-08 14:55:31 +0100

    Add form for sending snmp-requests

--- webapp/src/main/java/kth/se/exjobb/view/view/RequestBean.java
+++ webapp/src/main/java/kth/se/exjobb/view/view/RequestBean.java
@@ -7,12 +7,11 @@ package kth.se.exjobb.view.view;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import kth.se.exjobb.DTO.SetRequestDTO;
 import kth.se.exjobb.controller.Controller;
 import kth.se.exjobb.model.snmp.SNMPMessage;
 
@@ -26,47 +25,20 @@ public class RequestBean implements Serializable {
 
     @EJB
     private Controller contr;
-    private List<String> targets;
-    private String target;
     private boolean result = false;
-    private SNMPMessage alarm;
+    private SNMPMessage resultAlarm;
+    private SetRequestDTO setDTO = new SetRequestDTO();
+    private String ip;
+    
     public RequestBean() {
     }
     
     @PostConstruct
     public void init(){
-        targets = new ArrayList();
-        targets.add("Choose target");
-        targets.add("Simulated SNMP Agent");
-        targets.add("Localhost snmp service");
-    }
-    
-    public void sendRequest() throws IOException{        
-        switch(target){
-            case "Simulated SNMP Agent":
-                //alarm = contr.sendAlarm("127.0.0.1/9999");
-                result = true;
-                break;
-            case "Localhost snmp service":
-                //alarm = contr.sendAlarm("127.0.0.1/161");
-                result = true;
-        }
     }
 
-    public List<String> getTargets() {
-        return targets;
-    }
-
-    public void setTargets(List<String> targets) {
-        this.targets = targets;
-    }
-
-    public String getTarget() {
-        return target;
-    }
-
-    public void setTarget(String target) {
-        this.target = target;
+    public void sendRequest() throws IOException{
+        
     }
 
     public boolean isResult() {
@@ -77,12 +49,30 @@ public class RequestBean implements Serializable {
         this.result = result;
     }
 
-    public SNMPMessage getAlarm() {
-        return alarm;
+    public SNMPMessage getResultAlarm() {
+        return resultAlarm;
     }
 
-    public void setAlarm(SNMPMessage alarm) {
-        this.alarm = alarm;
+    public void setResultAlarm(SNMPMessage alarm) {
+        this.resultAlarm = alarm;
     }
+
+    public SetRequestDTO getSetDTO() {
+        return setDTO;
+    }
+
+    public void setSetDTO(SetRequestDTO setDTO) {
+        this.setDTO = setDTO;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+ 
     
 }
