86f9af8afbdc71da7b97560974dbfdb2a2ca8dc2
Marcus Blom <marcblom@kth.se>
2016-03-08 13:27:55 +0100

    Updated RelativeByteBuffer.java to also allow for creating an empty buffer and setting the data byte by byte.

--- webapp/src/main/java/kth/se/exjobb/model/util/RelativeByteBuffer.java
+++ webapp/src/main/java/kth/se/exjobb/model/util/RelativeByteBuffer.java
@@ -9,16 +9,44 @@ public class RelativeByteBuffer
 {
     byte[]  data;
     int     index;
-    
+   
+   /**
+    * Wraps a byte array in the relative byte buffer.
+    * @param data 
+    */
    public RelativeByteBuffer(byte[] data)
    {
        this.data = data;
        index = 0;
    }
    
+   /**
+    * Creates an empty buffer of specified length.
+    * @param bufferLength 
+    */
+   public RelativeByteBuffer(int bufferLength)
+   {
+       data = new byte[bufferLength];
+       index = 0;
+   }
+   
+   /**
+    * Gets current byte and increments the pointer.
+    * @return byte
+    */
    public byte getNext()
    {
        byte result = data[index++];
        return result;
+   }
+   
+   /**
+    * Sets the current byte and increments the pointer.
+    * @param b A byte of data to insert into the buffer.
+    */
+   public void setNext(byte b)
+   {
+       data[index] = b;
+       index++;
    }
 }
